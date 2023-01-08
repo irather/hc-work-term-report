4448965e366691302cbc93f8104a93c1f7d05f20
Ibrahim Rather <ibrahim.s.rather@gmail.com>
2023-01-08 18:03:00 -0500

    adding header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
     </div>
   );
 }
