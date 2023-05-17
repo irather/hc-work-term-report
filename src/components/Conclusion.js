adff963d276a4efb313fff1d38e4ac62eba7a3a0
Ibrahim Rather <ibrahim.s.rather@gmail.com>
2023-05-17 00:57:31 -0400

    updating conclusion

--- src/components/Conclusion.js
+++ src/components/Conclusion.js
@@ -8,9 +8,9 @@ function Conclusion() {
       <p>
         Overall, this co-op was a very valuable experience for me. I learned so
         much from a development stand point, and I was also able to improve a
-        huge amount on my React and CSS skills. I also learned a lot from a
-        behavourial stand point, being able to improve immensely on my problem
-        solving skills. In the end, I am very grateful to LINCS for providing me
+        huge amount on my React skills. I also learned a lot from a behavourial
+        stand point, being able to improve immensely on my problem solving
+        skills. In the end, I am very grateful to Health Canada for providing me
         with this opportunity and thankful I was able to make meaningful
         contributions for them!
       </p>
