7067675ba2c1e2eb40a1f0cb966916fba8b00162
Ibrahim Rather <ibrahim.s.rather@gmail.com>
2023-05-17 00:56:46 -0400

    updating job description

--- src/components/JobDesc.js
+++ src/components/JobDesc.js
@@ -6,46 +6,24 @@ function JobDesc() {
     <div id="jobDesc" className={styles.section}>
       <h2>Job Description</h2>
       <p>
-        I had one overall project to work on during my time at LINCS, and that
-        was to help design and build a website that would display what LINCS
-        was, what they did, the tools they created plus the documentation for
-        each tool, and also how a new user could get started with LINCS. My main
-        responsibility was to build the overall website and provide feedback on
-        the designs created by someone else. I will break down below what all on
-        the website I worked on and contributed to. The first thing I worked on
-        was creating the navigation bar for the website, I created multiple
-        navbar items and each item had to have it's own sidebar with it's own
-        content that I needed to create. The second and most interesting thing I
-        worked on was the home page, as this was the main thing that all users
-        of the website were going to see. Here, I created 6 different React
-        components. The first was the hero banner, which showed the mission
-        statement for LINCS, and buttons to take users to different parts of the
-        site. The second was a tile banner, meant as a way for users to see a
-        quick way of what LINCS does. The third component was expandable
-        drawers, which showcased different ways that users could maniplulate
-        their data. The fourth component was a carousel that displays the
-        different datasets that LINCS uses. The fifth component was a Term of
-        the Day displayer, that changed words upon refreshing the site. The
-        final component I made was cards which displayed blogs made by LINCS
-        employees. Some other fun things that I created on the site was a search
-        bar, implementing a dropdown that translated the full site from french
-        to english, implementing and modifying a glossary plugin that displays
-        terms on the site and their definitions along with a full glossary,
-        making the full site responsive for all screen sizes, and styling the
-        full site. Overall, majority of the skills that I used for this job I
-        had to learn on the job, as many of the technologies used I was not
-        fully experienced in or I hadn't seen it before. These skills included
-        mainly React and CSS, and one new tool I had to learn was a CMS called
-        Docusaurus, which is what I used to host the site on. The full site can
-        also be viewed here,{" "}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://portal.lincsproject.ca/"
-        >
-          https://portal.lincsproject.ca/
-        </a>
-        !
+        I had one overall project to work on during my time at Health Canada,
+        and that was to help build a Labaratory Information Management System
+        (LIMS) application. The overall goal with this project was to create new
+        workflow pages and create a sample submission table form. I created new
+        workflow pages using React by creating a component which showed the user
+        what step they were currently on and then having a table to show the
+        results of that step. The sample submission was the main task that I
+        worked on, for this task I created a UI of a table which would fill with
+        a file that the user could select and then the user would be able to
+        upload that table to the database. I also implemented visual error
+        checks for this table so that the user could see if the table they
+        uploaded matched the rule format which was predetermined. For example, I
+        created an error check to ensure the date format was always in
+        YYYY-MM-DD and if it wasn't, the full row would be highlighted in red.
+        Overall, majority of the skills that I used for this job I had to learn
+        on the job, as many of the technologies used I was not fully experienced
+        in or I hadn't seen it before. These skills included Axios which is
+        something that I was not familiar with from before.
       </p>
     </div>
   );
