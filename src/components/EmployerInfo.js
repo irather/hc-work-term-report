dda62c57297d27d27077c8b3cd6a18d3409cb47d
Ibrahim Rather <ibrahim.s.rather@gmail.com>
2023-05-16 21:31:53 -0400

    updating employer info

--- src/components/EmployerInfo.js
+++ src/components/EmployerInfo.js
@@ -6,12 +6,15 @@ function EmployerInfo() {
     <div id="employerInfo" className={styles.section}>
       <h2>Employer Information</h2>
       <p>
-        LINCS is a digital humanities research company located at the University
-        of Guelph. They use Semantic Technologies (linked data) to take Canadian
-        research and heritage data from across the web, converting and
-        connecting it, and making it more accessible to everyone. Essentially,
-        the goal of LINCS is giving Canadian researchers more opportunities to
-        explore and investigate culture in more transformative ways.
+        The Bureau of Microbial Hazards is a division that is located under
+        Health Canada. They work to minimize public health risks from the
+        consumption of foods contaminated with bacterial, parasitic, viral, and
+        prion-disease agents. They are able to do this by taking in samples from
+        certain areas and then using labs to identify and test what bacteria is
+        in the food. Their area of expertise include the following: microbial
+        pathogens, microbiological risk assessment & management, novel foods,
+        microbiological methods, microbial research, and food safety consumer
+        education.
       </p>
     </div>
   );
