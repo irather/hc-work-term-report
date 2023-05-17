824b218ecd541e871774ad672a8fdf5e0ffc3ef6
Ibrahim Rather <ibrahim.s.rather@gmail.com>
2023-05-16 20:46:33 -0400

    updating header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
-import lincsLogo from "../images/lincs-stacked-logo.png";
+import hcLogo from "../images/hc-logo.png";
 import styles from "../styles/header.module.css";
 
 function Header() {
   return (
     <div id="header" className={styles.headerContainer}>
-      <img src={lincsLogo} alt="" className={styles.heroImage} />
-      <h1>LINCS Work Term Report for S22 & F22</h1>
+      <img src={hcLogo} alt="" className={styles.heroImage} />
+      <h1>Health Canada Work Term Report for W23</h1>
     </div>
   );
 }
